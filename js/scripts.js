452dcc38bd809c602225b3edb0d277066322ebcf
abigailwambui <abigailw15njuguna@gmail.com>
2019-04-07 22:42:51 +0300

    Add appropriate changes to the summations of the rolled dice

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,12 @@
 // Business logic
 $(document).ready(function () {
-  var sum = 0
+  var sumOne = 0
+  var sumTwo = 0
     $("#abby").click(()=> {
 
   var randomOne = Math.floor((Math.random()*6) +1);
   $(".output1").text(randomOne);
-  sumOne = sum + randomOne;
+  sumOne = sumOne + randomOne;
   $(".output2").text(sumOne);
   //alert(random1);
 });
@@ -13,7 +14,7 @@ $("#wambui").click(()=> {
 
 var randomTwo = Math.floor((Math.random()*6) +1);
 $(".output3").text(randomTwo);
-sumTwo = sum + randomTwo;
+sumTwo = sumTwo + randomTwo;
 $(".output4").text(sumTwo);
 if (randomtwo===1) {
   alert(0);
